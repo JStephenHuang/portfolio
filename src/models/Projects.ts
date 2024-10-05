fcd5a19ecd551f6ffc4de3cd9c3cd34297e3821f
Waterloo Steph <waterloosteph@Stephens-MacBook-Air.local>
2024-10-05 17:02:14 -0400

    first commit

---
+++ src/models/Projects.ts
@@ -0,0 +1,46 @@
+export type Project = {
+    name: string;
+    role: string;
+    date: string;
+    link: string;
+    linkName: string;
+    src: string;
+}
+
+const capshun: Project = {
+    name : "Capshun",
+    role : "Co-Founder & Software Engineer",
+    date : "Jun. 2024 - Present",
+    link : "https://capshun.ca",
+    linkName :"www.capshun.ca",
+    src : "/capshun.mov"
+}
+
+const honeyice: Project = {
+    name : "HONEYÏCE",
+    role : "Freelance Software Developer",
+    date : "Jan. 2024 - Jul. 2024",
+    link : "https://honeyice.world",
+    linkName :"www.honeyice.world",
+    src : "/honeyice.mov"
+}
+const matrnaud: Project = {
+    name : "Matrnaud",
+    role : "Freelance Web Developer",
+    date : "Mar. 2023 - May 2023",
+    link : "https://matrnaud.com",
+    linkName :"www.matrnaud.com",
+    src : "/matrnaud.mov"
+}
+const marihacks: Project = {
+    name : "Marihacks",
+    role : "Website Lead Organizer",
+    date : "Aug. 2023 - Apr. 2024",
+    link : "https://marihacks.com",
+    linkName :"www.marihacks.com",
+    src : "/marihacks.mov"
+}
+
+export const projects: Project[] = [
+    capshun, honeyice, matrnaud, marihacks
+]

77923be483160cb2f393d5779417b1d6d483670d
Waterloo Steph <j.stephen.huang.developer@gmail.com>
2025-02-09 01:28:51 -0500

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-import Link from "next/link";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
-  title: "StephensPileOfStuff",
-  description: "Stephen's portfolio",
+  title: "Stephen's Pile of Stuff",
+  description: "Discover things about me.",
 };
 
 export default function RootLayout({
@@ -24,19 +11,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="w-full flex justify-center py-[5rem]">
-          <Link href={"/"} className="text-[2rem] text-center font-bold">
-            Stephen&apos;s pile of stuff
-          </Link>
-        </div>
-
-        {children}
-      </body>
-    </html>
-  );
+  return <html lang="en">{children}</html>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,3 @@
-import { projects } from "@/src/models/Projects";
-
-import ProjectCard from "@/components/ProjectCard";
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <div className="px-[5rem] flex flex-col items-center font-[family-name:var(--font-geist-sans)]">
-      <div className="grid grid-cols-3 gap-[1rem]">
-        {projects.map((project, key) => (
-          <ProjectCard key={key} project={project} />
-        ))}
-      </div>
-      <p className="mt-[5rem]">...</p>
-      <div className="py-[5rem] flex items-center gap-[1rem]">
-        <p>514-836-8531</p>|
-        <a className="hover:underline" href="mailto:jstephen.huang@uwaterloo.ca">
-          jstephen.huang@uwaterloo.ca
-        </a>
-        |
-        <a className="hover:underline" href="https://www.linkedin.com/in/jstephenhuang/">
-          Linkedin
-        </a>
-        |
-        <a className="hover:underline" href="https://github.com/JStephenHuang">
-          Github
-        </a>
-      </div>
-      <nav className="w-full flex items-start p-4">
-        <Link href={"/sheartak-tools-ltd"}>sheartak ltd</Link>
-      </nav>
-    </div>
-  );
+  return <div></div>;
 }
